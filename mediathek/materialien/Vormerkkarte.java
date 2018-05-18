0939da45bbfd1f342e07674f32d1f93e5b60bdc6
TuanAnhNguyen1121 <32867295+TuanAnhNguyen1121@users.noreply.github.com>
2018-05-19 00:18:01 +0200

    Add files via upload

---
+++ mediathek/materialien/Vormerkkarte.java
@@ -0,0 +1,49 @@
+package de.uni_hamburg.informatik.swt.se2.mediathek.materialien;
+
+import java.util.List;
+
+import de.uni_hamburg.informatik.swt.se2.mediathek.materialien.medien.Medium;
+
+public class Vormerkkarte
+{
+    private List<Kunde> _vormerker;
+    private final Medium _medium;
+
+    public Vormerkkarte(Medium medium)
+    {
+        _medium = medium;
+
+    }
+
+    public Kunde getErstenVormerker()
+    {
+        return _vormerker.get(0);
+    }
+
+    public boolean istVormerkenMöglich()
+    {
+        return false;
+    }
+
+    public List<Kunde> getAlleVormerker()
+    {
+        return _vormerker;
+
+    }
+
+    public void merkeVor(Kunde kunde)
+    {
+
+    }
+
+    public String getFormatiertenString()
+    {
+        return "";
+    }
+
+    public Medium getMedium()
+    {
+        return _medium;
+    }
+
+}
