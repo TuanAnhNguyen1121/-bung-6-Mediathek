aa122ab7bd94b15b848935bf11ee5c9972c976dd
jsntj <38441073+jsntj@users.noreply.github.com>
2018-05-25 11:02:35 +0200

    Update VormerkkarteTest.java

--- mediathek/materialien/VormerkkarteTest.java
+++ mediathek/materialien/VormerkkarteTest.java
@@ -1,6 +1,36 @@
 package de.uni_hamburg.informatik.swt.se2.mediathek.materialien;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNotSame;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import de.uni_hamburg.informatik.swt.se2.mediathek.fachwerte.Datum;
+import de.uni_hamburg.informatik.swt.se2.mediathek.fachwerte.Geldbetrag;
+import de.uni_hamburg.informatik.swt.se2.mediathek.fachwerte.Kundennummer;
+import de.uni_hamburg.informatik.swt.se2.mediathek.materialien.medien.CD;
+import de.uni_hamburg.informatik.swt.se2.mediathek.materialien.medien.Medium;
+
 public class VormerkkarteTest
 {
+   private List<Kunde> _vormerker;
+    private final Medium _medium;
+  
+  public VormerkkarteTest
+  {
+    
+  }
+      @Test
+  public void testegetFormatiertenString() throws Exception
+ {
+        assertNotNull(.getFormatiertenString());
+ }
+ 
 
+  
+  
+  
 }
