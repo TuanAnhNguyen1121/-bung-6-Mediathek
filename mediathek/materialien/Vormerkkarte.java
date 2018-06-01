b9ce71d3d1499c40ef771b07048f458a97f7e10a
PhilippAD97 <39409645+PhilippAD97@users.noreply.github.com>
2018-06-01 11:51:43 +0200

    Update Vormerkkarte.java

--- mediathek/materialien/Vormerkkarte.java
+++ mediathek/materialien/Vormerkkarte.java
@@ -40,15 +40,14 @@ public class Vormerkkarte {
 		_vormerker.add(kunde);
 
 	}
-
-	/**
-	 * Gibt den ersten Vormerker zurück.
-	 * 
-	 */
-	public Kunde getErstenVormerker() {
-		return _vormerker.get(0);
-	}
-
+	  public Kunde getVormerker(int i)
+	    
+   {	assert i != null: "Vorbedingung verleztz != null";
+	assert i <= 3 : "Vorbedingung verletzt: index <= 3";
+	assert i >=1 : "Vorbedingung verletzt: index <= 3";
+  
+     return _vormerker.get(i-1);
+     }
 	/**
 	 * Überprüft , ob der Kunde bereits ein Vormerker ist.
 	 * 
